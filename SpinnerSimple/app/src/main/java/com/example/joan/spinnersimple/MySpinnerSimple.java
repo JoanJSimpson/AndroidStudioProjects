b974e9ec520287fa8d8feb4df3d2e3d7cd001914
JoaN <milimetrosexual@gmail.com>
2015-11-02 20:55:23 +0100

    Modificado para que funcione en versiones anteriores de Api

--- SpinnerSimple/app/src/main/java/com/example/joan/spinnersimple/MySpinnerSimple.java
+++ SpinnerSimple/app/src/main/java/com/example/joan/spinnersimple/MySpinnerSimple.java
@@ -2,6 +2,7 @@ package com.example.joan.spinnersimple;
 
 
 import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -38,9 +39,12 @@ public void onCreate(Bundle savedInstanceState) {
                         String mensaje = "";
                         mensaje = "Has pulsado: " + semana[position];
                         showToast(mensaje);
-                    //no funciona en API 15, si en emulador API 16
+                    //no funciona en API 15, si en emulador API 21
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                         imagenFondo.setBackground(getDrawable(imagenes[position]));
-
+                    }else{
+                        imagenFondo.setBackgroundDrawable(getResources().getDrawable(imagenes[position]));
+                    }
 
 
                 }
